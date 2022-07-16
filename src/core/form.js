e5c8ea5003af8fef39b9fe79c352ecc21a18a718
Roman <ipromanone@gmail.com>
2022-07-16 22:07:13 +0300

    creating and validating forms

---
+++ src/core/form.js
@@ -0,0 +1,16 @@
+ export class Form {
+    constructor(form,controls) {
+        this.form = form,
+        this.controls = controls
+    }
+
+    value() {
+        const value = {}
+
+        Object.keys(this.controls).forEach(control => {
+            value[control] = this.form[control].value
+        })
+
+        return value
+    }
+ }
