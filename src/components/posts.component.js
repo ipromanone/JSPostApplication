ebe92eb6b8eb17518209f9bca5e053f76c7aa7dd
Roman <ipromanone@gmail.com>
2022-07-16 21:13:30 +0300

    finishing with navigation

---
+++ src/components/posts.component.js
@@ -0,0 +1,7 @@
+import { Component } from "../core/component";
+
+export class PostComponenet extends Component {
+    constructor(id) {
+        super(id)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import { HeaderComponent } from './components/header.component'
 import { NavigationComponent } from './components/navigation.component'
+import { CreateComponenet } from './components/create.component'
+import { FavoriteComponent } from './components/favorite.component'
+import { PostComponenet } from './components/posts.component'
 
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+
+const posts = new PostComponenet('posts')
+const create = new CreateComponenet('create')
+const favorite = new FavoriteComponent('favorite')
+
+navigation.registerTabs([
+    {name: 'create', component: create},
+    {name: 'posts', component: posts},
+    {name: 'favorite', component: favorite}
+])
