fd281372988e6396fd63753cdb9ae445e9d4d7c9
Roman <ipromanone@gmail.com>
2022-07-16 20:38:55 +0300

    navigation implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import {HeaderComponent} from './components/header.component'
+import { HeaderComponent } from './components/header.component'
+import { NavigationComponent } from './components/navigation.component'
 
-const header = new HeaderComponent('header')
 
-console.log(header)
+new HeaderComponent('header')
+
+const navigation = new NavigationComponent('navigation')
